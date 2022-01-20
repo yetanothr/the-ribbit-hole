90a876865b263fbe402695f719c051629d1af6e8
yetanother <hub2i77@gmail.com>
2022-01-20 15:07:42 +0100

    Created basic blinking animation

--- src/scripts/sky_anim.js
+++ src/scripts/sky_anim.js
@@ -8,21 +8,44 @@ let sky_ctx = sky_canv.getContext("2d");
 let sky_h = sky_canv.height;
 let sky_w = sky_canv.width;
 
-sky_ctx.fillStyle="rgb(0, 0, 0)";
-sky_ctx.fillRect(0,0,sky_w,sky_h);
-
 let stars = [];
+let dec = false;
 
 for(let i = 0; i < star_density; i++){
     stars.push(new particle(Math.floor(sky_w * Math.random()),Math.floor(sky_h * Math.random())));
-    sky_ctx.fillStyle="rgb(255, 255, 255)";
-    sky_ctx.fillRect(stars[i].x,stars[i].y,1,1);
 }
 
+requestAnimationFrame(drawSkyFrame)
 
 function particle(posX,posY){
     this.x = posX;
     this.y = posY;
+    this.col = 0;
+}
+function drawSkyFrame(){
+
+    sky_ctx.fillStyle="rgb(0, 0, 0)";
+    sky_ctx.fillRect(0,0,sky_w,sky_h);
+    
+    for(let i = 0; i < star_density; i++){
+
+        if(!dec){
+            stars[i].col++;
+            if(stars[i].col == 255)
+            dec = true;
+        }
+        else{
+            stars[i].col--;
+            if(stars[i].col == 0)
+            dec = false;
+        }
+        
+        sky_ctx.fillStyle=`rgb(${stars[i].col}, ${stars[i].col}, ${stars[i].col})`;
+        sky_ctx.fillRect(stars[i].x,stars[i].y,1,1);
+    }
+
+    requestAnimationFrame(drawSkyFrame);
+
 }
 
 function setCanvSize(){
